587c08cee882bbfa49690e49ed366a835b7e38d9
Ana Flávia Souza <ana.fsouza.santos@gmail.com>
2025-04-12 23:09:15 -0300

    fix: PredictionResponse schema

--- src/app/model_serving/get_client_data.py
+++ src/app/model_serving/get_client_data.py
@@ -1,25 +1,61 @@
-from typing import List, Dict
+import os
+import logging
+from typing import List, Dict, Any
 from ..config.redis_connection import RedisClient
 
 
-def get_predictions(model_name: str, client_codes: List[str], redis_client: RedisClient) -> Dict[str, dict]:
+
+def get_predictions(
+        model_name: str, 
+        client_codes: List[str], 
+    ) -> List[Dict[str, Any]]:
     """
-    Retrieve predictions from Redis for a list of customer codes and model name.
+    Retrieve predictions from Redis for a list of customer codes and a model name.
+
+    If the list of client codes is empty, fetch all keys from Redis that contain the model name
+    and use their suffixes (after the model_name prefix) as client codes.
 
     Args:
-        model_name (str): The name of the model (used as prefix in keys).
-        codes (List[str]): List of customer codes.
-        redis_client (RedisClient): Redis client instance.
+        model_name (str): The name of the model (used as a prefix in Redis keys).
+        client_codes (List[str]): List of customer codes to retrieve predictions for. If empty,
+                                  fetch all keys matching the model prefix.
+        redis_client (redis.StrictRedis): Redis client instance already connected.
 
     Returns:
-        Dict[str, str]: Dictionary mapping code to prediction data (JSON string or similar).
+        List[Dict[str, Any]]: List of dictionaries mapping full Redis keys to prediction values.
+                              Keys with no associated value will be returned with `None`.
+                              
     """
-    predictions = {}
-    for code in client_codes:
+    # Redis connection
+    host = os.getenv("REDIS_HOST")
+    password = os.getenv("REDIS_PASSWORD")
+
+    redis_client = RedisClient(host=host, password=password)
+    
+    if not client_codes:
+        try:
+            pattern = f"{model_name}:*"
+            keys = redis_client.keys(pattern)
+            client_codes_filter = [key.decode().split(":", 1)[1] for key in keys if key.decode().startswith(model_name)]
+        except Exception as e:
+            logging.error(f"Error fetching keys from Redis with pattern {pattern}: {e}")
+            return []
+    else:
+        client_codes_filter = client_codes
+
+    list_of_predictions = []
+
+    for code in client_codes_filter:
+        predictions = {}
         key = f"{model_name}:{code}"
-        value = redis_client.get(key)
-        if value is not None:
-            predictions[code] = value
-        else:
-            predictions[code] = None  
-    return predictions
+        try:
+            value = redis_client.get(key)
+            if value is not None:
+                predictions[key] = value.decode()  # Assuming value is stored as bytes
+            else:
+                predictions[key] = None
+            list_of_predictions.append(predictions)
+        except Exception as e:
+            logging.warning(f"Failed to retrieve data for {key} from Redis: {e}")
+
+    return list_of_predictions
